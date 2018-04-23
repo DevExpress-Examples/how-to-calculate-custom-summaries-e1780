84666c78f6fe5b183138698175d5410cd2048cdf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_CustomSummary/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.Data;
+
+namespace AgDataGrid_CustomSummary {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+        }
+
+        double totalPrice;
+        private void grid_CustomSummary(object sender, DevExpress.Data.CustomSummaryEventArgs e) {
+            if (e.SummaryProcess == CustomSummaryProcess.Start)
+                totalPrice = 0;
+            if (e.SummaryProcess == CustomSummaryProcess.Calculate) {
+                double price = (double)e.FieldValue;
+                int quantity = (int)e.GetValue("Quantity");
+                totalPrice += price * quantity;
+                e.TotalValue = totalPrice;
+            }
+        }
+    }
+}
---
+++ VB/AgDataGrid_CustomSummary/MainPage.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports DevExpress.Data
+
+Namespace AgDataGrid_CustomSummary
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = ProductList.GetData()
+		End Sub
+
+		Private totalPrice As Double
+		Private Sub grid_CustomSummary(ByVal sender As Object, ByVal e As DevExpress.Data.CustomSummaryEventArgs)
+			If e.SummaryProcess = CustomSummaryProcess.Start Then
+				totalPrice = 0
+			End If
+			If e.SummaryProcess = CustomSummaryProcess.Calculate Then
+				Dim price As Double = CDbl(e.FieldValue)
+				Dim quantity As Integer = CInt(Fix(e.GetValue("Quantity")))
+				totalPrice += price * quantity
+				e.TotalValue = totalPrice
+			End If
+		End Sub
+	End Class
+End Namespace
