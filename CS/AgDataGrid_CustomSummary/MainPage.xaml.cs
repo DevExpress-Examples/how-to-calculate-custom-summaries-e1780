86a3cf2a3aa2765fd712777bc7aee1100bcf67a8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_CustomSummary/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace AgDataGrid_CustomSummary {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+        }
+
+        double totalPrice;
+        private void grid_CustomSummary(object sender, DevExpress.AgData.CustomSummaryEventArgs e) {
+            if (e.SummaryProcess == DevExpress.AgData.CustomSummaryProcess.Start)
+                totalPrice = 0;
+                if (e.SummaryProcess == DevExpress.AgData.CustomSummaryProcess.Calculate) {
+                    double price = (double)e.FieldValue;
+                    int quantity = (int)e.GetValue("Quantity");
+                    totalPrice += price * quantity;
+                    e.TotalValue = totalPrice;
+                }
+        }
+    }
+}
---
+++ VB/AgDataGrid_CustomSummary/MainPage.xaml.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+
+Namespace AgDataGrid_CustomSummary
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = ProductList.GetData()
+		End Sub
+
+		Private totalPrice As Double
+		Private Sub grid_CustomSummary(ByVal sender As Object, _
+                                        ByVal e As DevExpress.AgData.CustomSummaryEventArgs)
+			If e.SummaryProcess = _
+                                                        DevExpress.AgData.CustomSummaryProcess.Start Then
+				totalPrice = 0
+			End If
+				If e.SummaryProcess = _
+                                                                         DevExpress.AgData.CustomSummaryProcess.Calculate Then
+					Dim price As Double = CDbl(e.FieldValue)
+					Dim quantity As Integer = CInt(Fix(e.GetValue("Quantity")))
+					totalPrice += price * quantity
+					e.TotalValue = totalPrice
+				End If
+		End Sub
+	End Class
+End Namespace
